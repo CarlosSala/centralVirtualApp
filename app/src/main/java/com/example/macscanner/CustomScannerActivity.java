319bea89cb8de1e354c872ce541c4222b112ed0b
Carlos <carlos.salazar.mu@usach.cl>
2019-09-29 19:19:14 -0300

    primer commit

---
+++ app/src/main/java/com/example/macscanner/CustomScannerActivity.java
@@ -0,0 +1,142 @@
+package com.example.macscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.ViewfinderView;
+
+import java.util.Random;
+
+public class CustomScannerActivity extends AppCompatActivity implements DecoratedBarcodeView.TorchListener {
+
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    private Button switchFlashlightButton;
+    private ViewfinderView viewfinderView;
+    private String CodeScanned;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_scanner);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CodeScanned = getIntent().getExtras().getString("title", "QR");
+
+        //toolbar.setTitleTextColor(Color.rgb(200,200,200));
+        setTitle(CodeScanned);
+
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setBackgroundColor(Color.rgb(45,45,45));
+
+        barcodeScannerView = (DecoratedBarcodeView) findViewById(R.id.zxing_barcode_scanner);
+        barcodeScannerView.setTorchListener(this);
+
+        switchFlashlightButton = (Button) findViewById(R.id.switch_flashlight);
+
+        viewfinderView = (ViewfinderView) findViewById(R.id.zxing_viewfinder_view);
+
+        Drawable img = this.getResources().getDrawable( R.drawable.ic_flash );
+        img.setBounds( 0, 0, 60, 60 );
+        switchFlashlightButton.setCompoundDrawables( img, null, null, null );
+
+        // if the device does not have flashlight in its camera,
+        // then remove the switch flashlight button...
+        if (!hasFlash()) {
+            switchFlashlightButton.setVisibility(View.GONE);
+        }
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+
+        changeMaskColor(null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Check if the device's camera has a Flashlight.
+     *
+     * @return true if there is Flashlight, otherwise false.
+     */
+    private boolean hasFlash() {
+        return getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public void switchFlashlight(View view) {
+        if (getString(R.string.turn_on_flashlight).equals(switchFlashlightButton.getText())) {
+            barcodeScannerView.setTorchOn();
+        } else {
+            barcodeScannerView.setTorchOff();
+        }
+    }
+
+    public void changeMaskColor(View view) {
+        Random rnd = new Random();
+        int color = Color.argb(100, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        // viewfinderView.setMaskColor(color);
+    }
+
+    @Override
+    public void onTorchOn() {
+        switchFlashlightButton.setText(R.string.turn_off_flashlight);
+        //  switchFlashlightButton.setBackground(ContextCompat.getDrawable(this, R.drawable.ic_flash_off));
+        Drawable img = this.getResources().getDrawable( R.drawable.ic_flash_off );
+        img.setBounds( 0, 0, 60, 60 );
+        switchFlashlightButton.setCompoundDrawables( img, null, null, null );
+
+    }
+
+    @Override
+    public void onTorchOff() {
+        switchFlashlightButton.setText(R.string.turn_on_flashlight);
+
+        // switchFlashlightButton.setBackground(ContextCompat.getDrawable(this, R.drawable.ic_flash));
+        Drawable img = this.getResources().getDrawable( R.drawable.ic_flash );
+        img.setBounds( 0, 0, 60, 60 );
+        switchFlashlightButton.setCompoundDrawables( img, null, null, null );
+    }
+
+}
---
+++ app/src/main/java/com/example/macscanner/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.macscanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_scan);
+        textView = findViewById(R.id.tv1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customScanner(view);
+            }
+        });
+
+    }
+
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(this, QrActivity.class);
+                intent.putExtra("codeScanned", result.getContents());
+                startActivity(intent);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    private void customScanner(View view) {
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+
+        integrator.setBeepEnabled(true);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.EAN_13);
+        integrator.setPrompt("");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBarcodeImageEnabled(true);
+        integrator.setCaptureActivity(CustomScannerActivity.class);
+        integrator.setTimeout(20000);
+        integrator.addExtra("title", "MACs SCANNER");
+        integrator.initiateScan();
+    }
+}
