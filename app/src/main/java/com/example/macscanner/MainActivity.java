07deb499b1b1453b821992762d638301c7fe9aac
Carlos <carlos.salazar.mu@usach.cl>
2019-09-30 16:31:14 -0300

    se agrego conexion con firebase

--- app/src/main/java/com/example/macscanner/MainActivity.java
+++ app/src/main/java/com/example/macscanner/MainActivity.java
@@ -1,20 +1,40 @@
 package com.example.macscanner;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    public static final String AUTHOR_KEY = "author";
+    public static final String QUOTE_KEY = "quote";
+    public static final String TAG = "InspiringQuote";
+
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().collection("sampleData").document("inspiration");
+
+    private Button button, button_firebase;
     private TextView textView;
 
     @Override
@@ -22,13 +42,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       // FirebaseApp.initializeApp(this);
         button = findViewById(R.id.btn_scan);
         textView = findViewById(R.id.tv1);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 customScanner(view);
+            }
+        });
+
+
+        button_firebase = findViewById(R.id.btn_firebase);
+
+        button_firebase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveQuote(view);
             }
         });
 
@@ -67,5 +99,39 @@ public class MainActivity extends AppCompatActivity {
         integrator.setTimeout(20000);
         integrator.addExtra("title", "MACs SCANNER");
         integrator.initiateScan();
+    }
+
+
+
+    public void saveQuote(View view){
+
+        EditText quoteView = (EditText)findViewById(R.id.editTextQuote);
+        EditText authorView = (EditText)findViewById(R.id.editTextAuthor);
+
+        String quoteText = quoteView.getText().toString();
+        String authorText = authorView.getText().toString();
+
+
+        if (quoteText.isEmpty() || authorText.isEmpty()){
+            return;
+        }
+
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put(QUOTE_KEY, quoteText);
+        dataToSave.put(AUTHOR_KEY, authorText);
+
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "Document has been saved!");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Document was not saved!", e);
+
+            }
+        });
+
     }
 }
