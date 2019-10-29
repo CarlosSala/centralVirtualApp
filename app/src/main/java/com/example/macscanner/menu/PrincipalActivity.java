ca2c716169810403dc055f22e95fec2664dbb26c
Carlos <carlos.salazar.mu@usach.cl>
2019-10-29 18:14:44 -0300

    se incorporó informacion a los textview y un enlace de cierre de sesion al navigation drawer

--- app/src/main/java/com/example/macscanner/menu/PrincipalActivity.java
+++ app/src/main/java/com/example/macscanner/menu/PrincipalActivity.java
@@ -2,9 +2,14 @@ package com.example.macscanner.menu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,35 +19,48 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.example.macscanner.LoginActivity;
 import com.example.macscanner.R;
 import com.example.macscanner.menu.addMac.addMacActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class PrincipalActivity extends AppCompatActivity {
+public class PrincipalActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private final static String TAG = "PrincipalActivity";
+
+    private FirebaseAuth firebaseAuth;
+
+    private FirebaseFirestore firestoredb;
 
     private DrawerLayout drawerLayout;
 
     private Button btn_add_mac, btn_logout;
-
-    private FirebaseAuth firebaseAuth;
+    private TextView tv_user_name, tv_user_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
-
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
-
         actionBarDrawerToggle.syncState();
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+
 
         btn_add_mac = findViewById(R.id.btn_add_mac);
 
@@ -63,6 +81,47 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });
 
+        View header=navigationView.getHeaderView(0);
+        tv_user_name = header.findViewById(R.id.tv_user_name);
+        tv_user_email = header.findViewById(R.id.tv_user_email);
+
+        GetUserData();
+    }
+
+    private void GetUserData() {
+
+        firestoredb = FirebaseFirestore.getInstance();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+
+            String email = user.getEmail();
+
+            DocumentReference docRef = firestoredb.collection("users").document(email);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+
+                            String name = document.getString("nombre");
+                            String email = document.getString("email");
+
+                            tv_user_name.setText(name);
+                            tv_user_email.setText(email);
+                            //Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
     }
 
 
@@ -78,7 +137,7 @@ public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
@@ -86,5 +145,20 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_about) {
+
+
+        } else if (id == R.id.nav_close_session){
+            Logout();
+
+        }
+
+        return false;
+    }
 }
 
